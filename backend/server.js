0e9d1eebd6906990760baa6a4c82a8346bb6e8ce
Suarchaipoolchart <suarchaipoolchart@gmail.com>
2026-03-29 00:06:39 +0700

    fix api route

--- backend/server.js
+++ backend/server.js
@@ -145,7 +145,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/auth", authRoutes);
 app.use("/api/restaurants", restaurantRoutes);
-app.use("/foods", foodRoutes);
+app.use("/api/foods", foodRoutes);
 app.use("/api/cart", cartRoutes);
 app.use("/api/orders", orderRoutes);
 app.use("/api/users", userRoutes);
