389722c53576a4c10e0588ec31771999005b8fa7
Suarchaipoolchart <suarchaipoolchart@gmail.com>
2026-03-29 03:55:10 +0700

    final fix api + routing

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: import.meta.env.VITE_API_URL + "/api"
+  baseURL: "https://food-delivery-xgdk.onrender.com",
 });
 
 // 🔥 ใส่ token อัตโนมัติ
