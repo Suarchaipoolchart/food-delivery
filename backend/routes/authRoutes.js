beef421d099d2062589b9d40ec3fd78cc95d7789
Suarchaipoolchart <suarchaipoolchart@gmail.com>
2026-03-30 00:08:07 +0700

    update

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { registerUser, loginUser, forgotPassword, resetPassword } from "../controllers/authController.js";
-import AdminLogin from "./pages/AdminLogin";
+
 
 console.log("forgotPassword:", forgotPassword)
 
@@ -10,7 +10,7 @@ router.post("/register", registerUser);
 router.post("/login", loginUser);
 router.post("/forgot-password", forgotPassword);
 router.post("/reset-password", resetPassword);
-router.post("/admin-login", AdminLogin);
+
 
 router.get("/test",(req,res)=>{
 res.send("auth route working")
